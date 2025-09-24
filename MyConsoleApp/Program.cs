c84ce07a217be0cc4199cc1df0c3a223b47c5be1
Ignacio Menchaca <JoseIgnacio.MenchacaCastro@mx.bosch.com>
2025-09-24 11:19:16 -0600

    a

--- MyConsoleApp/Program.cs
+++ MyConsoleApp/Program.cs
@@ -1,26 +1,35 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿// String filtering
 string[] words = ["Bosch", "Mexico", "Mango", "Mark", "Blr", "Clean code"];
 
 bool FilterWith(string word, string c) => word.StartsWith(c, StringComparison.OrdinalIgnoreCase);
 
-var result = FilterString(words, s => FilterWith(s, "M"));
+var filteredStrings = Filter(words, s => FilterWith(s, "M"));
 
-foreach (string w in result)
+foreach (string w in filteredStrings)
 {
 	Console.WriteLine(w);
 }
 
+// Integer filtering
+var numbers = new[] { 1, 2, 3, 4, 5, 6 };
 
-List<string> FilterString(string[] strings, Func<string, bool> predicate)
+bool IsEven(int n) => n % 2 == 0;
+
+var filteredInts = Filter(numbers, IsEven);
+
+foreach (int i in filteredInts)
 {
-	var filterString = new List<string>();
-	foreach (var s in strings)
+	Console.WriteLine(i);
+}
+
+List<T> Filter<T>(IEnumerable<T> items, Func<T, bool> predicate)
+{
+	var result = new List<T>();
+	foreach (var item in items)
 	{
-		if (predicate(s))
-		{
-			filterString.Add(s);
-		}
+		if (predicate(item))
+			result.Add(item);
 	}
 
-	return filterString;
+	return result;
 }
