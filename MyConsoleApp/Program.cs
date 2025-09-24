d2107d1970d42006be59f6263de316229d52179b
Ignacio Menchaca <JoseIgnacio.MenchacaCastro@mx.bosch.com>
2025-09-24 09:13:03 -0600

    a

---
+++ MyConsoleApp/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+string[] words = ["Bosch", "Mexico", "Mango", "Mark", "Blr", "Clean code"];
+
+var result = words
+	.Where(word => word.StartsWith('M'))
+	.ToArray();
+
+foreach (string w in result)
+{
+	Console.WriteLine(w);
+}
